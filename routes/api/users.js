6eb00882611a834062829fd8de8df18c98f63fe6
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2021-02-12 23:58:27 -0500

    API routes file written and register and login and user from models folder required

---
+++ routes/api/users.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bscryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+
+const User = require("../../models/User")
