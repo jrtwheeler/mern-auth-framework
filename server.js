9d08aa6f38de5d6b1f00b8551ce158ebe7ab32a7
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2021-02-13 00:10:49 -0500

    Passport.js file is written

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.use(bodyParser.json());
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, {
+    useNewUrlParser: true, useUnifiedTopology: true
+  })
   .then(() => console.log("MongoDB succesfully connected"))
   .catch((err) => console.log(err));
 
